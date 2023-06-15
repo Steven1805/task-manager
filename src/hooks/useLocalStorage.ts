29f7e6b86675540f0e721a6af22bcfb80da187ff
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2023-06-15 11:33:15 +0300

    feat: add a hook for local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,18 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from "react";
 
-}
+type ValueSetter<T> = (value: T) => void;
 
-export {
-  useLocalStorage
-}
+const useLocalStorage = <T>(key: string, initialValue: T): [T, ValueSetter<T>] => {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
